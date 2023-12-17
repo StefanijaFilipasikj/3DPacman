9bef7fe0032735e1adb1d865cc95ba28bea3ae35
Stefanija <stefanija.filipashikj@students.finki.ukim.mk>
2023-12-17 21:19:33 +0100

    Add assimp folder

--- cmake-build-debug-visual-studio/vendor/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/drist/Desktop/Github/Opengl/3D-Pacman/vendor/assimp/contrib/zlib
-# Build directory: C:/Users/drist/Desktop/Github/Opengl/3D-Pacman/cmake-build-debug-visual-studio/vendor/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/CMakeFiles/OpenGLPrj.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for OpenGLPrj.
-# This may be replaced when dependencies are built.
